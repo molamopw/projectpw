52148723e761dca74eaa69e4da20127f67dd1ef6
molamopw <116250407+molamopw@users.noreply.github.com>
2022-10-21 12:51:00 +0800

    Update pricewatch.py

--- pricewatch.py
+++ pricewatch.py
@@ -16,34 +16,34 @@ def emitRequest(url):
     else:
       raise Exception(r.status_code, url)
 
-def getPricewatch():
-    # define output name
-    PRICEWATCH_ITEMLIST = 'pricewatch_itemlist.json'
-    PRICEWATCH_MAP_ITEMLIST = 'pricewatch_map_itemlist.json'
 
-    if path.isfile(PRICEWATCH_LIST):
-      os.remove(PRICEWATCH_LIST)
-    if path.isfile(PRICEWATCH_MAP_LIST):
-      os.remove(PRICEWATCH_MAP_LIST)
-      
-    itemList = {}
-    r = emitRequest('https://online-price-watch.consumer.org.hk/opw/opendata/pricewatch.json')
-    itemList = []
-        
-    for item in r.json():
-        brandList = []
-        brandList.append({
-            "en": route['brand']['en'],
-            "tc": route['brand']['zh-Hant'],
-            "sc": route['brand']['zh-Hans']
-        })
+# define output name
+PRICEWATCH_ITEMLIST = 'pricewatch_itemlist.json'
+PRICEWATCH_MAP_ITEMLIST = 'pricewatch_map_itemlist.json'
+
+if path.isfile(PRICEWATCH_LIST):
+  os.remove(PRICEWATCH_LIST)
+if path.isfile(PRICEWATCH_MAP_LIST):
+  os.remove(PRICEWATCH_MAP_LIST)
+
+itemList = {}
+r = emitRequest('https://online-price-watch.consumer.org.hk/opw/opendata/pricewatch.json')
+itemList = []
+
+for item in r.json():
+    brandList = []
+    brandList.append({
+        "en": route['brand']['en'],
+        "tc": route['brand']['zh-Hant'],
+        "sc": route['brand']['zh-Hans']
+    })
+
+    itemList.append({
+        "code": route['code'],
+        "brand": brandList
+    })
     
-        itemList.append({
-            "code": route['code'],
-            "brand": brandList
-        })
-    with open(PRICEWATCH_ITEMLIST, 'w') as f:
-    f.write(json.dumps(itemList, ensure_ascii=False))
+with open(PRICEWATCH_ITEMLIST, 'w') as f:
+f.write(json.dumps(itemList, ensure_ascii=False))
     
-getPricewatch()
     
